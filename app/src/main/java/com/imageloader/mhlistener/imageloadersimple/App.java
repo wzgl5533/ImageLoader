a6a92dedad370f27231621a9d1664a068d423666
qlh <708937822@qq.com>
2018-06-27 12:38:31 +0800

    创建

---
+++ app/src/main/java/com/imageloader/mhlistener/imageloadersimple/App.java
@@ -0,0 +1,22 @@
+package com.imageloader.mhlistener.imageloadersimple;
+
+import android.app.Application;
+
+import com.imageloader.mhlistener.imageloaderlib.ImageLoader;
+
+/**
+ * Created by JohnsonFan on 2017/12/9.
+ */
+
+public class App extends Application {
+
+	public static App gApp;
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		gApp = this;
+		//初始化图片库
+		ImageLoader.getInstance().setImageLoader(new PicassoLoader());
+	}
+}
